27081604a7c098402690374f1c9d6104f5189b2d
Charles Merritt <chazzmerritt2@gmail.com>
2021-04-12 16:54:34 -0600

    updated gitignor, code not in working order. added a debuging terminal

--- stablizer1/stablizer1.ino
+++ stablizer1/stablizer1.ino
@@ -9,7 +9,7 @@
 #define chip_select 10
 #define servo_read 0
 #define servo_write 3
-#define cal_sig 1
+#define cal_sig 2
 #define setpoint2angle 1.0 / 180
 
 Servo servo;
@@ -19,16 +19,26 @@ int cal_val = 0;
 
 
 void makeCal(void);
-void logData(int setpoint, double plane_angle);
+void logData(int setpoint, int roll_ctrl, double plane_angle);
 double getPlaneAngle(void);
+String file = "Log.csv";
 
 
 void setup() {
-  servo.attach(servo_write);
+  //servo.attach(servo_write);
   SD.begin(chip_select);
   mpu.begin();
   mpu.setAccelerometerRange(MPU6050_RANGE_4_G);
-  mpu.setFilterBandwidth(MPU6050_BAND_94_HZ);
+  mpu.setFilterBandwidth(MPU6050_BAND_44_HZ);
+  pinMode(cal_sig, INPUT);
+  digitalWrite(cal_sig, HIGH);
+  int cnt = 0;
+  file = "Log_" + String(cnt) + ".csv";
+  while(SD.exists(file))
+  {
+    cnt++;
+    file = "Log_" + String(cnt) + ".csv";
+  }
 }
 
 void loop() {
@@ -36,7 +46,7 @@ void loop() {
   int setpoint = analogRead(servo_read);
   int calibrate = digitalRead(cal_sig);
 
-  if (calibrate)
+  if (!calibrate)
   {
     makeCal();
   }
@@ -46,12 +56,13 @@ void loop() {
     int roll_ctrl = 0;
     double plane_angle = getPlaneAngle();
     double desired_angle = setpoint * setpoint2angle;
+    roll_ctrl = (setpoint-137)*(160.0/328.0) + 10;
   
     // write servo
-    servo.write(roll_ctrl);
+    // servo.write(roll_ctrl);
   
     // house keeping
-    logData(setpoint, plane_angle);
+    logData(calibrate*180, cal_val, plane_angle);
     delay(10);
   }
 }
@@ -73,11 +84,12 @@ void makeCal(void)
   cal_val = temp / itter;
 }
 
-void logData(int setpoint, double plane_angle)
+void logData(int setpoint, int roll_ctrl, double plane_angle)
 {
   String data_string = String(setpoint)+",";
+  data_string += String(roll_ctrl)+",";
   data_string += String(plane_angle)+",";
-  log_file = SD.open("Log.csv", FILE_WRITE);
+  log_file = SD.open(file, FILE_WRITE);
   log_file.println(data_string);
   log_file.close();
 }
@@ -87,5 +99,5 @@ double getPlaneAngle(void)
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
   double angle = atan2(a.acceleration.x, a.acceleration.y);
-  return angle - cal_val;
+  return angle * 180.0 / PI - cal_val;
 }
